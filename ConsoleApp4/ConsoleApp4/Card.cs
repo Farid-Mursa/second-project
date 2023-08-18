a7d6f72d89e394bb77eef0eca8cad2f0b6efe0ac
Farid-Mursa <117182057+Farid-Mursa@users.noreply.github.com>
2023-08-18 16:45:44 +0400

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal abstract class Card
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string CardNumber { get; private set; }
+        public int CVV { get; private set; }
+        public decimal Balance { get ; set; }
+
+        public abstract decimal Increase(decimal increase);
+        public abstract decimal Extract(decimal decrease);
+
+        public Card(string name, string surname, string cardNumber, int cVV, decimal balance)
+        {
+            Name = name;
+            Surname = surname;
+            CardNumber = cardNumber;
+            CVV = cVV;
+            Balance = balance;
+        }
+    }
+}
